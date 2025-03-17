15d9a978377ad51c2ec71e71712ffce4349f53cd
Gersi.doko <Gersi.doko@unh.edu>
2025-03-17 11:43:59 -0400

    need to work on confidence intervals

--- code/cvar_vs_qcvar_experiment.jl
+++ code/cvar_vs_qcvar_experiment.jl
@@ -5,32 +5,55 @@ using Random
 using Dates
 Random.seed!(1234)
 
+"""
+  run_one_experiment(n::Int, x, p, α)
+
+  Run one experiment with n samples and random variable *x* with probability distribution 
+  *p* and risk level *α* and return the results.
+
+Returns:
+  NTuple{2, Float64}:
+    slow_cvar_result: The CVaR time taken result.
+    fast_cvar_result: The qCVaR time taken result.
+"""
+function run_one_experiment(x, p, α)
+  local start = now()
+  slow_cvar_result = CVaR(deepcopy(x), p, α).value
+  slow_time = (now() - start).value
+  start = now()
+  fast_cvar_result = qCVaR!(deepcopy(x), p, α).value
+  fast_time = (now() - start).value
+  local δ = abs(slow_cvar_result - fast_cvar_result)
+  if δ >= 1e-6
+    println("CVaR: $slow_cvar_result, qCVaR: $fast_cvar_result, diff: $δ")
+    error("Results are not equal!")
+  end
+  (slow_cvar_time=slow_time, fast_cvar_time=fast_time)
+end
+
 stop = 100000000
 step = 1000000
 start = step
+# Number of trials per experiment
+# One experiment is running the CVaR and qCVaR algorithms and collecting the time taken.
+trials = 10
 experiments = range(start=start, stop=stop, step=step)
 len = length(experiments)
-qcvar_results = zeros(Float64, len)
-cvar_results = zeros(Float64, len)
+qcvar_results = zeros(Float64, len * trials)
+cvar_results = zeros(Float64, len * trials)
 # @threads :dynamic for i ∈ 1:len
 for i ∈ 1:len
+  GC.enable(false)
   n = Int(ceil(experiments[i]))
   println("Experiment $i / $len --- n = $n")
   x = rand(Float64, n) .* 100
   p = rand(Float64, n)
   p ./= sum(p)
   α = 0.6
-  GC.enable(false)
-  local start = now()
-  slow_cvar_result = CVaR(deepcopy(x), p, α).value
-  cvar_results[i] = (now() - start).value
-  start = now()
-  fast_cvar_result = qCVaR!(deepcopy(x), p, α).value
-  qcvar_results[i] = (now() - start).value
-  local δ = abs(slow_cvar_result - fast_cvar_result)
-  if δ >= 1e-6
-    println("CVaR: $slow_cvar_result, qCVaR: $fast_cvar_result, diff: $δ")
-    error("Results are not equal!")
+  for j ∈ 0:trials-1
+    s, f = run_one_experiment(x, p, α)
+    qcvar_results[i+j] = f
+    cvar_results[i+j] = s
   end
   GC.enable(true)
   x = nothing
